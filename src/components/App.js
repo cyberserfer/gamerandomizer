a6ac84a72a4e1c27285b0782e3fc84c2e32bdf00
Paul Buss <cyberserfer@gmail.com>
2020-02-19 16:05:22 -0600

    working on selector fix

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,9 @@ class App extends Component {
 
     updateEventValue(e) {
         this.setState({ [e.target.name]: e.target.value });
+        console.log("in updateEventValue")
+        console.log(e.target.name)
+        console.log(e.target)
 
     }
 
@@ -27,15 +30,16 @@ class App extends Component {
             this.setState({ submitSwitch: true })
         }
 
-    // handleChange = (selectedOption) => {
-    //     this.setState({ selectedOption });
-    //     }
+        // handleChange = (selectedOption) => {
+        //     this.setState({ selectedOption });
+        //     }
 
     }
 
 
     render() {
-
+        console.log("dataType:")
+        console.log(this.state.dataType)
         return (
             <div style={{ marginLeft: '25px' }}>
                 <header>
@@ -45,16 +49,16 @@ class App extends Component {
                     <div>Select encounter type:</div>
                     <select>
                         <option name="dataType" value="TalolanEncounter" onChange={event => this.updateEventValue(event)} checked={(this.state.dataType === 'TalolanEncounter') ? true : false}>Talolan Encounter </option>
-            <br />
-                        <option name="dataType" value="RandomEncounter"  onChange={event => this.updateEventValue(event)} checked={(this.state.dataType === 'RandomEncounter') ? true : false}>Random Encounter </option>
+                        <br />
+                        <option name="dataType" value="RandomEncounter" onChange={event => this.updateEventValue(event)} checked={(this.state.dataType === 'RandomEncounter') ? true : false}>Random Encounter </option>
                     </select>
                     <div>Enter number of results to return</div>
                     <input name="numberOfResults" onKeyPress={this.handleSubmit} onBlur={event => this.updateEventValue(event)} ></input>
                     <button name="submit" onClick={this.handleSubmit}>Submit</button>
 
                     <div>
-                        {(this.state.dataType === "TalolanEncounter") &&  <TalolanEncounter results={this.state.numberOfResults} />}
-                        {(this.state.dataType === "RandomEncounter") &&  <RandomEncounter results={this.state.numberOfResults} />}
+                        {(this.state.dataType === "TalolanEncounter") && <TalolanEncounter results={this.state.numberOfResults} />}
+                        {(this.state.dataType === "RandomEncounter") && <RandomEncounter results={this.state.numberOfResults} />}
                     </div>
 
                 </div>
